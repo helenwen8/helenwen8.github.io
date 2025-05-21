a185c2b334861722ee9d6869a378c1e96c8280f3
Helen Huang <helen_wenwen@live.com>
2025-05-20 22:11:06 -0400

    fixed dark mode on resume

--- src/components/CollapsibleExp.tsx
+++ src/components/CollapsibleExp.tsx
@@ -30,11 +30,11 @@ const CollapsibleExp: React.FC<Props> = ({
     return (
         <div className="mb-6">
         <div className="flex justify-between cursor-pointer" onClick={() => setIsCollapsed(!isCollapsed)}>
-            <h3 className="text-xl font-semibold text-gray-700">
+            <h3 className="text-xl font-semibold">
                 <span>{isCollapsed ? '▶' : '▼'} </span>
                 {title}
             </h3>
-            <h3 className="end text-l font-semibold text-gray-700">{dates}</h3>
+            <h3 className="end text-l font-semibold">{dates}</h3>
         </div>
 
         <div
@@ -43,19 +43,19 @@ const CollapsibleExp: React.FC<Props> = ({
             style={{ maxHeight: isCollapsed ? '0' : undefined }}
         >
             <div className="flex justify-between">
-                <span className="text-gray-700 font-semibold">{company}</span>
-                <span className="end text-gray-600 text-sm">{location}</span>
+                <span className="font-semibold">{company}</span>
+                <span className="end text-sm">{location}</span>
             </div>
             
             <ul className="list-disc list-inside my-2">
                 {description.map((item, idx) => (
-                    <li key={idx} className="text-gray-800">{item}</li>
+                    <li key={idx} className="">{item}</li>
                 ))}
             </ul>
 
             {skills && (
                 <div className="mt-2">
-                    <span className="font-semibold text-gray-600">Technologies:</span>
+                    <span className="font-semibold">Technologies:</span>
                     <div className="flex flex-wrap gap-2 mt-1">
                     {skills.map((tech, idx) => (
                         <span key={idx} className="bg-blue-100 text-blue-800 px-2 py-1 rounded-full text-xs font-medium">
